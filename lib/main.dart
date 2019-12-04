774cc3ede12e30af8114c94da07d23c945af0253
Adrianne Peredo <aperedo.uth702@gmail.com>
2019-12-04 11:47:11 +0800

    Add lesson 18

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(MaterialApp(
   home: QuoteList(),
@@ -10,10 +11,10 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
-  List<String> quotes = [
-    'Be yourself; everyone else is already taken',
-    'I have nothing to declare except my genius',
-    'The truth is rarely pure and never simple'
+  List<Quote> quotes = [
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
+    Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
+    Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple')
   ];
 
   @override
@@ -26,10 +27,8 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text(quote)).toList()
-//        children: quotes.map((quote) {
-//          return Text(quote);
-//        }).toList(),
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+        crossAxisAlignment: CrossAxisAlignment.start,
       ),
     );
   }
