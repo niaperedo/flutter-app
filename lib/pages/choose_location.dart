4b97a886844736d6622922f19ce6da92df3305dc
Ian Peredo <aperedo.uth702@gmail.com>
2019-12-05 17:24:27 +0800

    Add lesson 25

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -7,10 +7,23 @@ class ChooseLocation extends StatefulWidget {
 class _ChooseLocationState extends State<ChooseLocation> {
   int counter = 0;
 
+  void getData() async {
+     String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'yoshi';
+    });
+
+     String bio = await Future.delayed(Duration(seconds: 3), () {
+       return 'noshi';
+     });
+
+     print('$username - bio');
+  }
+
   @override
   void initState() {
     super.initState();
-    print('Init State');
+    getData();
+    print('hey there!');
   }
 
   @override
