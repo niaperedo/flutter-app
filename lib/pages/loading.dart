bde218ece94c5a1390555329771e983a6aea66ea
Ian Peredo <aperedo.uth702@gmail.com>
2019-12-06 15:19:29 +0800

    Add lesson 27

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -8,16 +8,19 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
-    Response resp = await get('https://jsonplaceholder.typicode.com/todos/1');
+  void getTime() async {
+    Response resp = await get('https://worldtimeapi.org/api/timezone/Asia/Manila');
     Map data = jsonDecode(resp.body);
-    print(data);
+
+    DateTime now = DateTime.parse(data['datetime']);
+    now = now.add(Duration(hours: int.parse(data['utc_offset'].substring(1,3))));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
