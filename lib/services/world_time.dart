9f89fa556dfe489e8493061f03d327e34fbad496
Ian Peredo <aperedo.uth702@gmail.com>
2019-12-06 16:21:08 +0800

    Add lesson 29

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -10,12 +10,18 @@ class WorldTime {
   WorldTime({this.location, this.flag, this.url});
 
   Future<void> getTime() async {
-    Response resp = await get('https://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(resp.body);
+    try {
+      Response resp = await get('https://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(resp.body);
 
-    DateTime now = DateTime.parse(data['datetime']);
-    now = now.add(Duration(hours: int.parse(data['utc_offset'].substring(1,3))));
+      DateTime now = DateTime.parse(data['datetime']);
+      now = now.add(Duration(hours: int.parse(data['utc_offset'].substring(1,3))));
 
-    time = now.toString();
+      time = now.toString();
+    } catch (e) {
+
+      time = 'could not get data';
+      print(e);
+    }
   }
 }
