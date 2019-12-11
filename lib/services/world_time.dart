6a9d2c2a09bb0ed3889fbcb163d0ef48d4d860e9
Ian Peredo <aperedo.uth702@gmail.com>
2019-12-11 14:19:20 +0800

    Add lesson 31

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   String location;
@@ -17,7 +18,7 @@ class WorldTime {
       DateTime now = DateTime.parse(data['datetime']);
       now = now.add(Duration(hours: int.parse(data['utc_offset'].substring(1,3))));
 
-      time = now.toString();
+      time = DateFormat.jm().format(now);
     } catch (e) {
 
       time = 'could not get data';
